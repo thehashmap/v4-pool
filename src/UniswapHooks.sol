dfbbf9326458f4411518fa89a3233456d6088889
thehashmap <nkmnikhil0009@gmail.com>
2023-10-12 22:45:16 +0530

    remove unnecessary hooks

--- src/UniswapHooks.sol
+++ src/UniswapHooks.sol
@@ -19,12 +19,12 @@ contract UniswapHooks is BaseHook, IHookFeeManager, IDynamicFeeManager {
         return Hooks.Calls({
             beforeInitialize: true,
             afterInitialize: true,
-            beforeModifyPosition: true,
-            afterModifyPosition: true,
-            beforeSwap: true,
-            afterSwap: true,
-            beforeDonate: true,
-            afterDonate: true
+            beforeModifyPosition: false,
+            afterModifyPosition: false,
+            beforeSwap: false,
+            afterSwap: false,
+            beforeDonate: false,
+            afterDonate: false
         });
     }
 
@@ -57,117 +57,5 @@ contract UniswapHooks is BaseHook, IHookFeeManager, IDynamicFeeManager {
     {
         console2.log("afterInitialize");
         return IHooks.afterInitialize.selector;
-    }
-
-    /// @inheritdoc IHooks
-    function beforeModifyPosition(
-        address, // sender
-        IPoolManager.PoolKey calldata, // key
-        IPoolManager.ModifyPositionParams calldata // params
-    )
-        external
-        pure
-        override
-        returns (bytes4)
-    {
-        console2.log("beforeModifyPosition");
-        return IHooks.beforeModifyPosition.selector;
-    }
-
-    /// @inheritdoc IHooks
-    function afterModifyPosition(
-        address, // sender
-        IPoolManager.PoolKey calldata, // key
-        IPoolManager.ModifyPositionParams calldata, // params
-        BalanceDelta // delta
-    )
-        external
-        pure
-        override
-        returns (bytes4)
-    {
-        console2.log("afterModifyPosition");
-        return IHooks.afterModifyPosition.selector;
-    }
-
-    /// @inheritdoc IHooks
-    function beforeSwap(
-        address, // sender
-        IPoolManager.PoolKey calldata, // key
-        IPoolManager.SwapParams calldata // params
-    )
-        external
-        pure
-        override
-        returns (bytes4)
-    {
-        console2.log("beforeSwap");
-        return IHooks.beforeSwap.selector;
-    }
-
-    /// @inheritdoc IHooks
-    function afterSwap(
-        address, // sender
-        IPoolManager.PoolKey calldata, // key
-        IPoolManager.SwapParams calldata, // params
-        BalanceDelta // delta
-    )
-        external
-        pure
-        override
-        returns (bytes4)
-    {
-        console2.log("afterSwap");
-        return IHooks.afterSwap.selector;
-    }
-
-    /// @inheritdoc IHooks
-    function beforeDonate(
-        address, // sender
-        IPoolManager.PoolKey calldata, // key
-        uint256, // amount0
-        uint256 // amount1
-    )
-        external
-        pure
-        override
-        returns (bytes4)
-    {
-        console2.log("beforeDonate");
-        return IHooks.beforeDonate.selector;
-    }
-
-    /// @inheritdoc IHooks
-    function afterDonate(
-        address, // sender
-        IPoolManager.PoolKey calldata, // key
-        uint256, // amount0
-        uint256 // amount1
-    )
-        external
-        pure
-        override
-        returns (bytes4)
-    {
-        console2.log("afterDonate");
-        return IHooks.afterDonate.selector;
-    }
-
-    /// @inheritdoc IHookFeeManager
-    function getHookSwapFee(IPoolManager.PoolKey calldata) external pure returns (uint8) {
-        console2.log("getHookSwapFee");
-        return 100;
-    }
-
-    /// @inheritdoc IHookFeeManager
-    function getHookWithdrawFee(IPoolManager.PoolKey calldata) external pure returns (uint8) {
-        console2.log("getHookWithdrawFee");
-        return 100;
-    }
-
-    /// @inheritdoc IDynamicFeeManager
-    function getFee(IPoolManager.PoolKey calldata) external pure returns (uint24) {
-        console2.log("getFee");
-        return 10_000;
     }
 }
